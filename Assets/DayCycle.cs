bd266dcc2dbee47602314ada7fe98155a1a30e07
mareq022 <mareq022@gmail.com>
2021-03-14 16:27:42 +0100

    Light changing and time mechanics change

--- Assets/DayCycle.cs
+++ Assets/DayCycle.cs
@@ -6,63 +6,76 @@ using UnityEngine;
 public class DayCycle : MonoBehaviour
 {
     Light light;
-    int hour;
-    int minutes;
-    float beginingTime;
-    float time;
-    float lastTime;
+    [Header("Time")]
+    int time;
+    int beginingTime = 630;//420;
+    float beginingTimeSeconds;
+    float seconds;
+    float lastTimeSeconds;
+    int maxTime = 1440;
     [SerializeField]
     float dayLengthInMinutes=15;
-    [SerializeField]
-    int beginingHour=7;
-    [SerializeField]
-    int endHour=24;
     [Header("Light")]
-    [SerializeField]
-    float morningLight = 0.5f;
-    [SerializeField]
-    float nightLight = 0;
     [SerializeField]
     float dayLight = 0.75f;
     [SerializeField]
-    float sunDownHour = 22;
+    int sunUpTime = 5*60;
+    [SerializeField]
+    int sunDownTime = 22*60;
+    bool changingLight;
 
     // Start is called before the first frame update
     void Start()
     {
         light = GetComponent<Light>();
-        hour = beginingHour;
-        minutes = 0;
-        beginingTime = Time.time;
         time = beginingTime;
-        light.intensity = morningLight;
+        beginingTimeSeconds = Time.time;
+        seconds = beginingTimeSeconds;
+        light.intensity = 0.5f;
     }
 
 
     // Update is called once per frame
     void Update()
     {
-        float time = Time.time - lastTime;
-        if (time - beginingTime > dayLengthInMinutes/(endHour-beginingHour))
+        seconds = Time.time - lastTimeSeconds;
+        if (seconds - beginingTimeSeconds > dayLengthInMinutes/24)
         {
-            minutes++;
-            lastTime = Time.time;
-            Debug.Log(hour + " : " + minutes);
+            time++;
+            lastTimeSeconds = Time.time;
         }
-        if(minutes==60)
+        if(time==1440)
         {
-            hour++;
-            minutes = 0;
-        }
-        if (hour == 24)
-        {
-            hour = 0;
+            time = 0;
         }
         //Light stuff
+        if (time == 200) ChangeLight(0.25f, 60);
+        if (time == sunUpTime - 120) ChangeLight(0.6f, 120);
+        if (time == 11*60) ChangeLight(dayLight, 60);
+        if (time == 16*60) ChangeLight(0.55f, 60);
+        if (time == 18*60) ChangeLight(0.4f, 120);
+        if (time == sunDownTime - 120) ChangeLight(0, 120);
+    }
 
-        //Getting brighter      
-        if (hour > 4 && hour < 12) light.intensity = morningLight + (dayLight - morningLight) * (hour / 12f);
-        //Getting darker
-        if (hour > 12 && hour < sunDownHour) light.intensity = nightLight + dayLight * (sunDownHour - hour)/(sunDownHour-12);
+    void ChangeLight(float lightChange, float changeTime)
+    {
+        if(!changingLight)
+        {
+            float startLight = light.intensity;
+            float changeTimeInSeconds = changeTime * dayLengthInMinutes / 24;
+            StartCoroutine(ChangeLightCO(startLight, lightChange, changeTimeInSeconds));
+        }
+    }
+
+    IEnumerator ChangeLightCO(float startLight, float lightChange, float changeTimeInSeconds)
+    {
+        changingLight = true;
+        for(int i = 0; i <= 100; i++)
+        {
+            light.intensity += (lightChange - startLight) / 100f;       
+            yield return new WaitForSeconds(changeTimeInSeconds / 100f);
+        }
+        light.intensity = lightChange;
+        changingLight = false;
     }
 }
